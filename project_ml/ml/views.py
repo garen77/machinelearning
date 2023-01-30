c11a7808da83b089bda5b9a674977e52dd5efc82
creo.cons <creo.cons@bene.it>
2023-01-30 21:18:46 +0100

    added json

--- project_ml/ml/views.py
+++ project_ml/ml/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
+from django.http import JsonResponse
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the ml index.")
+    
+def classify(request):
+    return JsonResponse({'status': 'Classifying...'})
